10e888e798ac418322ea231ce5ea5b798b3d8718
Yxxnsu <dustn0121@naver.com>
2021-10-09 00:29:38 +0900

    [ADD] custom dialog

---
+++ flutter_firebase_practice/lib/app/modules/home/custom_dialog.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomDialog {
+  static void showDialog() {
+    Get.dialog(
+      WillPopScope(
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
+        onWillPop: () => Future.value(false),
+      ),
+      barrierDismissible: false,
+      barrierColor: const Color(0xff141A31).withOpacity(.3),
+      useSafeArea: true,
+    );
+  }
+
+  static void dismiss() {
+    Get.back();
+  }
+}
