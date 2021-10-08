57cdaccf3da3607203bf73a4edaf4166a1b4ea61
Yxxnsu <dustn0121@naver.com>
2021-10-09 00:30:46 +0900

    [ADD] main.dart

--- flutter_firebase_practice/lib/main.dart
+++ flutter_firebase_practice/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_practice/home.dart';
+import 'package:flutter_firebase_practice/app/routes/app_pages.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,12 +15,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GetMaterialApp(
+      title: 'Firebase CRUD',
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xff141A31),
+        primaryColorDark: const Color(0xff081029),
+        scaffoldBackgroundColor: const Color(0xff141A31),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.yellow,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.yellow,
+        ),
       ),
-      home: const HomePage(),
     );
   }
 }
