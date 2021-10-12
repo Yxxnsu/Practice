f34d022ff156220b0b21b664a526e4c6c1251732
Yxxnsu <dustn0121@naver.com>
2021-10-13 01:07:06 +0900

    [ADD] main.dart

--- flutter_animaiton/lib/main.dart
+++ flutter_animaiton/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animaiton/views/test_view.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,16 +17,63 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeView(),
+      home: const AnimationSelectView(),
     );
   }
 }
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+class AnimationSelectView extends StatefulWidget {
+  const AnimationSelectView({Key? key}) : super(key: key);
+
+  @override
+  State<AnimationSelectView> createState() => _AnimationSelectViewState();
+}
+
+class _AnimationSelectViewState extends State<AnimationSelectView> {
+  bool _trigger = true;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AnimatedPhysicalModel(
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                  ),
+                  shape: BoxShape.rectangle,
+                  elevation: _trigger ? 10 : 20,
+                  color: _trigger ? Colors.black : Colors.yellow,
+                  shadowColor: _trigger ? Colors.blue : Colors.red,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.bounceInOut,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() {
+                    _trigger = !_trigger;
+                  }),
+                  child: const Text("AnimatedPhysicalModel Start"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () => Get.to(() => const TestView()),
+                  child: Text('Go to TestScreen'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
