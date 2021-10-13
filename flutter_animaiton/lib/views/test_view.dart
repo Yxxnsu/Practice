633ed6d24f0a498c8b26169b76a50305cb779861
Yxxnsu <dustn0121@naver.com>
2021-10-13 12:57:09 +0900

    [ADD] testview.dart

---
+++ flutter_animaiton/lib/views/test_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TestView extends StatefulWidget {
+  const TestView({Key? key}) : super(key: key);
+
+  @override
+  _TestViewState createState() => _TestViewState();
+}
+
+class _TestViewState extends State<TestView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TestView'),
+      ),
+      body: Container(),
+    );
+  }
+}
