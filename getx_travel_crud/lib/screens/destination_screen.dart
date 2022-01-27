4b82b421486e9086e1efb1616569233c8b882c7d
Yxxnsu <dustn0121@naver.com>
2022-01-27 13:57:41 +0900

    [ADD] destination_screen.dart

---
+++ getx_travel_crud/lib/screens/destination_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DestinationScreen extends StatelessWidget {
+  const DestinationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
