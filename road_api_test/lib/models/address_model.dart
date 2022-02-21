41b4afe524f6d5dbcf624b3f3b5493414901235f
Yxxnsu <dustn0121@naver.com>
2022-02-21 16:34:36 +0900

    [FIX] debuging

--- road_api_test/lib/models/address_model.dart
+++ road_api_test/lib/models/address_model.dart
@@ -1,10 +1,9 @@
 import 'package:road_api_test/models/common_model.dart';
 import 'package:road_api_test/models/juso_model.dart';
-import 'dart:developer' as developer;
 
 class Address {
   final Common common;
-  final List<Juso> jusoList;
+  final List<Juso>? jusoList;
   Address({
     required this.common,
     required this.jusoList,
@@ -13,27 +12,18 @@ class Address {
   Map<String, dynamic> toMap() {
     return {
       'common': common.toMap(),
-      'jusoList': jusoList.map((x) => x.toMap()).toList(),
+      'jusoList': jusoList == null 
+        ? null
+        : jusoList!.map((x) => x.toMap()).toList(),
     };
   }
 
   factory Address.fromJson(Map<String, dynamic> json) {
-
-    final results = json['results'];
-    // print(results['juso'] == null);
-    List<Juso> jusoList = [];
-    print(jusoList == null);
-
-    if(results['juso'] != null && results['common'] != null){
-      final jusoJusoList = results['juso'] as List;
-      jusoList = jusoJusoList.map((item) => Juso.fromJson(item)).toList();
-      // developer.log('${jusoJusoList.length}');
-      // jusoJusoList.map((e)=> developer.log('eeeeeeeeeeee'));
-      // jusoJusoList.forEach((element)=> developer.log('eeeeeeeeeeee'));
-    }
     return Address(
-      common: Common.fromJson(json['common']),
-      jusoList: jusoList,
+      common: Common.fromJson(json["results"]['common']),
+      jusoList: json["results"]["juso"] == null 
+        ? null
+        : List<Juso>.from(json["results"]["juso"].map((x)=> Juso.fromJson(x)))
     );
   }
 }
